c39f3dec658af95cd244dc944874de125d81de22
emmanuel-jan <emmanueljan80@gmail.com>
2022-10-23 18:39:51 +0300

    initial commit

---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Navbar from '../../Components/Navbar/Navbar'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>  
+    </div>
+  )
+}
+
+export default HomePage
